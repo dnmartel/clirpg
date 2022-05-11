e11fc77ab2d03a6fac42284073460e93655d02fd
dnmartel <marteld1992@gmail.com>
2022-05-10 23:58:51 -0300

    Inicio del index y pruebas de animación

---
+++ src/index.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-index").addEventListener( "click", ()=>{
+    document.getElementById("section-index").classList.remove("height-100");
+    document.getElementById("section-index").classList.add("height-0");
+    document.getElementById("game-mode").classList.remove("height-0");
+    document.getElementById("game-mode").classList.add("height-100");
+})
+
+
